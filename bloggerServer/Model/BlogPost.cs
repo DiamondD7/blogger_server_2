93894b3859dcc0789a6b79b4c636a5e93c3e324c
diamond <aaron.diamond2@yahoo.co.nz>
2023-05-29 19:13:01 +1200

    added all the necessary values for the post model

---
+++ bloggerServer/Model/BlogPost.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bloggerServer.Model
+{
+    public class BlogPost
+    {
+        [Key]
+        public int PostId { get; set; }
+
+        [Required]
+        public string PostTitle { get; set; }
+        public string? PostBody { get; set; }
+        public string? Description { get; set; }
+        public IFormFile PostImage { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+
+    }
+}
