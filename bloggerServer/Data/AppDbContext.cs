a3b081e159a3c874c618cda5bc95987c2e77a3a5
diamond <aaron.diamond2@yahoo.co.nz>
2023-05-29 19:17:48 +1200

    implemented db context in the server

---
+++ bloggerServer/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using bloggerServer.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace bloggerServer.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options) : base(options)
+        {
+
+        }
+
+
+
+        public DbSet<User> Users { get; set; } 
+        public DbSet<BlogPost> Blogs { get; set; }
+    }
+}
