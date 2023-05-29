88dd772440a6e9af4ee7ec2a7f30dc73923fb929
diamond <aaron.diamond2@yahoo.co.nz>
2023-05-29 18:59:16 +1200

    added user models

---
+++ bloggerServer/Model/SecurityQuestion.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bloggerServer.Model
+{
+    public class SecurityQuestion
+    {
+        [Required]
+        public string SecurityQuestionOne { get; set; }
+        [Required]
+        public string SecurityQuestionTwo { get; set; }
+        [Required]
+        public string SecurityQuestionThree { get; set; }
+
+
+
+
+        [Required]
+        private string SecurityAnswerOne { get; set; }
+
+        [Required]
+        private string SecurityAnswerTwo { get; set; }
+
+        [Required]
+        private string SecurityAnswerThree { get; set; }
+    }
+}
---
+++ bloggerServer/Model/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bloggerServer.Model
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        [Required]
+        public string UserEmail { get; set; }
+        [Required]
+        public string UserMobileNumber { get; set; }
+        public string UserGender { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
+        public string UserUserName { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string UserPassword { get; set; }
+
+        public List<SecurityQuestion> SecurityQuestions { get; set; }
+    }
+}
